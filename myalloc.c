77369d87d16fd2531db1a1f4e737804197ae32c4
Danielle Puga <danielle.puga@gmail.com>
2022-02-12 12:02:35 -0800

    project document code added

--- myalloc.c
+++ myalloc.c
@@ -1,13 +1,47 @@
+#include "myalloc.h"
 
-
-// myalloc() function
+// the goal is to make a myalloc() function
 // will allocate certain # of bytes and return a
 // pointer to it.
 // NO using builtin malloc.
 // intentionally incomplete for now
 
+struct block *head = NULL;  // Head of the list, empty
+
+void *myalloc(int bytes)
+{
+  if (head == NULL)
+  {
+    // if this is the first call, sbrk() to get space
+    head = sbrk(1024);
+    head->next = NULL;
+    head->size = 1024 - PADDED_SIZE(sizeof(struct block));
+    head->in_use = 0;
+  }
+}
+
+// below from project document
+void print_data(void)
+{
+    struct block *b = head;
+
+    if (b == NULL) {
+        printf("[empty]\n");
+        return;
+    }
+    while (b != NULL) {
+        // Uncomment the following line if you want to see the pointer values
+        //printf("[%p:%d,%s]", b, b->size, b->in_use? "used": "free");
+        printf("[%d,%s]", b->size, b->in_use? "used": "free");
+        if (b->next != NULL) {
+            printf(" -> ");
+        }
+        b = b->next;
+    }
+    printf("\n");
+}
+
 // logic:
-// if this is the first call, sbrk() to get space
 // > at the same time, we will make a linked list node inside
 // > this new space indicating its siez and "in use" status.
 
---
+++ myalloc.h
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define ALIGNMENT 16   // Must be power of 2
+#define GET_PAD(x) ((ALIGNMENT - 1) - (((x) - 1) & (ALIGNMENT - 1)))
+#define PADDED_SIZE(x) ((x) + GET_PAD(x))
+#define PTR_OFFSET(p, offset) ((void*)((char *)(p) + (offset)))
+
+struct block {
+    struct block *next;
+    int size;     // Bytes
+    int in_use;   // Boolean
+};
