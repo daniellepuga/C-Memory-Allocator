e6c9d2744917e6d852f1e10ea7ff444f64d0b8ad
Danielle Puga <danielle.puga@gmail.com>
2022-02-13 10:43:48 -0800

    added helper function, new variables to cut repetition

--- myalloc.c
+++ myalloc.c
@@ -6,18 +6,97 @@
 // NO using builtin malloc.
 // intentionally incomplete for now
 
-struct block *head = NULL;  // Head of the list, empty
+struct block * head = NULL;  // Head of the list, empty
+
+// example runs from project document
+int main(void)
+{
+  void *p;
+    // print_data();
+    // p = myalloc(16);
+    // print_data();
+    // p = myalloc(16);
+    // printf("%p\n", p);
+    // should return:
+    // [empty]
+    // [1008, used]
+
+    print_data();
+    p = myalloc(32);
+    print_data();
+    // should return:
+    // [empty]
+    // [1008, used]
+    // 0x0
+}
 
 void *myalloc(int bytes)
 {
   if (head == NULL)
   {
-    // if this is the first call, sbrk() to get space
+    // if this is the first call, sbrk() to get space (1024)
+    // > at the same time, we will make a linked list node inside
+    // > this new space indicating its size and "in use" status.
     head = sbrk(1024);
-    head->next = NULL;
     head->size = 1024 - PADDED_SIZE(sizeof(struct block));
     head->in_use = 0;
+    head->next = NULL;
+
+  // we look to see if memory is available
+  if (PADDED_SIZE(bytes) <= head->size)
+  {
+    // when memory is available, we make the next block's head
+    // have memory equal to the size of the block plus a buffer
+    // at the head.
+    block * next = PTR_OFFSET(head, TOTAL_PAD);
+    allocate_memory(&head, &next, bytes);
+    return PTR_OFFSET(head, PAD_BLOCK_SIZE);
   }
+  }
+  else
+  {
+    // walk the linked list in a look and look for the first
+    // node that is not in use and big enough to hold amt+ some
+    block * cur = head->next;
+    while (cur)
+    {
+      if(!cur->in_use && (PADDED_SIZE(bytes) + PAD_BLOCK_SIZE) <= cur->size)
+      {
+        // if the block is found:
+        // > mark it in use
+        // > return a pointer to the user data just after the linked
+        // list node (plus some padding).
+        block * next = PTR_OFFSET(cur, TOTAL_PAD);
+        allocate_memory(&cur, &next, bytes);
+        return PTR_OFFSET(cur, PAD_BLOCK_SIZE);
+      }
+      else
+      {
+        cur = cur->next;
+      }
+    }
+  }
+  // if NO space found:
+  return NULL;
+}
+
+void allocate_memory(block ** current, block ** next, int bytes) {
+    block * cur = *current;
+    block * nxt = *next;
+
+    nxt->in_use = false;
+    // we make the next node's size equal to current's size minus
+    // the padding
+    nxt->size = cur->size - (TOTAL_PAD);
+    // next should have no next value yet
+    nxt->next = NULL;
+    // we set the current node's next equal to next node
+    cur->next = nxt;
+    // we know that our current node is in use
+    cur->in_use = true;
+    // we update the current node's size to be equal to the padding
+    cur->size = PADDED_SIZE(bytes);
+    // we create a new node and expect that the next one is not in use
 }
 
 // below from project document
@@ -31,7 +110,7 @@ void print_data(void)
     }
     while (b != NULL) {
         // Uncomment the following line if you want to see the pointer values
-        //printf("[%p:%d,%s]", b, b->size, b->in_use? "used": "free");
+        // printf("[%p:%d,%s]", b, b->size, b->in_use? "used": "free");
         printf("[%d,%s]", b->size, b->in_use? "used": "free");
         if (b->next != NULL) {
             printf(" -> ");
@@ -40,40 +119,3 @@ void print_data(void)
     }
     printf("\n");
 }
-
-// example runs from project document
-int main(void)
-{
-  void *p;
-    print_data();
-    p = myalloc(16);
-    print_data();
-    p = myalloc(16);
-    printf("%p\n", p);
-    // should return:
-    // [empty]
-    // [1008, used]
-
-    // print_data();
-    // p = myalloc(64);
-    // print_data();
-    // should return:
-    // [empty]
-    // [1008, used]
-    // 0x0
-}
-
-// logic:
-// > at the same time, we will make a linked list node inside
-// > this new space indicating its siez and "in use" status.
-
-// walk the linked list in a look and look for the first
-// node that is not in use and big enough to hold amt+ some
-
-// if the block is found:
-// > mark it in use
-// > return a pointer to the user data just after the linked
-// list node (plus some padding).
-
-// if NO block found:
-// > return NULL
--- myalloc.h
+++ myalloc.h
@@ -1,14 +1,24 @@
+// myalloc.h
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
+#include <stdbool.h>
 
 #define ALIGNMENT 16   // Must be power of 2
 #define GET_PAD(x) ((ALIGNMENT - 1) - (((x) - 1) & (ALIGNMENT - 1)))
 #define PADDED_SIZE(x) ((x) + GET_PAD(x))
 #define PTR_OFFSET(p, offset) ((void*)((char *)(p) + (offset)))
+#define PAD_BLOCK_SIZE ( (int) PADDED_SIZE(sizeof(block)) )
+#define TOTAL_PAD (PAD_BLOCK_SIZE + PADDED_SIZE(bytes))
 
-struct block {
+typedef struct block {
     struct block *next;
-    int size;     // Bytes
-    int in_use;   // Boolean
-};
+    int size;   // Bytes
+    int in_use; // Boolean
+} block;
+
+void * myalloc(int);
+void print_data(void);
+void allocate_memory(block **, block **, int);
